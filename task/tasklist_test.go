383eddc4c0f00fde0164b84c6cb249bbfc258e7d
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-21 15:13:17 +0100

    Add out of bounds test for Del function

--- task/tasklist_test.go
+++ task/tasklist_test.go
@@ -4,18 +4,33 @@ import (
 	"testing"
 )
 
-func TestDel(t *testing.T) {
-	ts := []Task{
+func getFilledTasklist() *Tasklist {
+	tasks := []Task{
 		Task{"Task No. 1"},
 		Task{"Task No. 2"},
 		Task{"Task No. 3"},
 		Task{"Task No. 4"},
 		Task{"Task No. 5"},
 	}
-	list := Tasklist{Title: "Tasks", Tasks: ts}
+	return &Tasklist{Title: "Tasks", Tasks: tasks}
+}
+
+func TestDel(t *testing.T) {
+	list := getFilledTasklist()
 	list.Del([]int{0, 2, 4})
 	numOfTasks := list.Len()
 	if numOfTasks != 2 {
 		t.Error("Expected 2, got ", numOfTasks)
 	}
+}
+
+func TestDelOutOfBounds(t *testing.T) {
+	list := getFilledTasklist()
+	// catch out of bounds panic
+	defer func() {
+		if recover() != nil {
+			t.Fail()
+		}
+	}()
+	list.Del([]int{-1, 100000})
 }
