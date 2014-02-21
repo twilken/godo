62e07f48d2d593d28444df91043bfd1d1b3129bd
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-21 14:37:14 +0100

    Update Del function test

--- task/tasklist_test.go
+++ task/tasklist_test.go
@@ -5,27 +5,17 @@ import (
 )
 
 func TestDel(t *testing.T) {
-	/*
-		tasks := []Task{
-			Task{"Task No. 1"},
-			Task{"Task No. 2"},
-			Task{"Task No. 3"},
-			Task{"Task No. 4"},
-			Task{"Task No. 5"},
-		} */
-	var tasks *Tasklist
-	tasks = &Tasklist{Title: "Tasks"}
-	tasks.Add([]string{"Task1", "Task2", "Task3", "Task4", "Task5"})
-	tasks.Del([]int{0, 2, 4})
-	/*
-		list := Tasklist{}
-		list.Add([]string{"Task1", "Task2", "Task3", "Task4", "Task5"})
-		list.Del([]int{1, 3, 5})
-		if list.Len() != 2 {
-			t.Fail()
-		}
-	*/
-	if tasks.Len() != 2 {
-		t.Fail()
+	ts := []Task{
+		Task{"Task No. 1"},
+		Task{"Task No. 2"},
+		Task{"Task No. 3"},
+		Task{"Task No. 4"},
+		Task{"Task No. 5"},
+	}
+	list := Tasklist{Title: "Tasks", Tasks: ts}
+	list.Del([]int{0, 2, 4})
+	numOfTasks := list.Len()
+	if numOfTasks != 2 {
+		t.Error("Expected 2, got ", numOfTasks)
 	}
 }
