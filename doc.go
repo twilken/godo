8a263951b7ca15e60d282f4245573231dab3bc35
Thimo <tenpeoplemeet@gmail.com>
2014-12-02 17:26:39 +0100

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,42 @@
+/*
+godo
+
+Yet another simple command line todo list.
+
+Usage
+
+godo [subcommand] [arguments to subcommand]
+
+The following subcommands are available:
+ add, a		Add one or more tasks
+ del, d		Delete one or more tasks by their number
+ list, l		Show a list of all tasks
+ help, h		Show help text
+
+There is a short version for every subcommand.
+
+Examples
+
+Show all tasks:
+ godo list
+
+Add three tasks:
+ godo add "Get milk" Workout "Dump TV"
+
+Delete tasks at position 1 and 2:
+ godo del 1 2
+
+Print help information:
+ godo help
+
+Save file location
+
+By default the godo save file is in your `HOME` directory. You can change that by creating a
+`GODOPATH` environment variable and setting it to another path. Note that, for now, the directory must
+exist before godo can use it. The actual file is always named `.godo`.
+
+Why
+
+For me this is part of learning about **git**, **github** and **golang**.
+*/
+package main
